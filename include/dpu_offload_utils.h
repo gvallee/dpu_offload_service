0d2cba5ce5a62b87cea13ced376abc8c6809c718
Geoffroy Vallee <geoffroy.vallee@gmail.com>
2022-02-03 10:50:11 -0500

    Changes to make it easier to support many clients

---
+++ include/dpu_offload_utils.h
@@ -0,0 +1,19 @@
+//
+// Copyright (c) 2022, NVIDIA CORPORATION. All rights reserved.
+//
+// See LICENSE.txt for license information
+//
+
+#ifndef DPU_OFFLOAD_UTILS_H
+#define DPU_OFFLOAD_UTILS_H
+
+#if defined(c_plusplus) || defined(__cplusplus)
+#    define _EXTERN_C_BEGIN extern "C" {
+#    define _EXTERN_C_END   }
+#else
+#    define _EXTERN_C_BEGIN
+#    define _EXTERN_C_END
+#endif
+
+
+#endif // DPU_OFFLOAD_UTILS_H
