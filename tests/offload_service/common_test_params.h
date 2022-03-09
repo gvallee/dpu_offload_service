555d41adfdbd3ada3f0b84e619cc639209064f33
Geoffroy Vallee <geoffroy.vallee@gmail.com>
2022-03-09 15:41:43 -0500

    Improve the test of notifications

---
+++ tests/offload_service/common_test_params.h
@@ -0,0 +1,12 @@
+//
+// Copyright (c) 2022, NVIDIA CORPORATION. All rights reserved.
+//
+// See LICENSE.txt for license information
+//
+
+#ifndef _COMMON_TEST_PARAMS_H_
+#define _COMMON_TEST_PARAMS_H_
+
+#define NUM_TEST_EVTS (42)
+
+#endif // _COMMON_TEST_PARAMS_H_
