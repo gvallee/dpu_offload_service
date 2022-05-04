42563a35818e16b509569028f0f2ccfad224fa06
Geoffroy Vallee <geoffroy.vallee@gmail.com>
2022-05-04 15:42:56 -0700

    Set config to engine so everything can be properly freed
    Signed-off-by: Geoffroy Vallee <geoffroy.vallee@gmail.com>

--- tests/comms/self_comm.c
+++ tests/comms/self_comm.c
@@ -16,7 +16,7 @@
 
 #define MY_TEST_NOTIF_ID (1000)
 #define MY_TEST_MANY_NOTIFS_ID (1001)
-#define NUM_NOTIFS (1000)
+#define NUM_NOTIFS (1)
 
 static bool self_notif_received = false;
 static int self_notification_cb(struct dpu_offload_ev_sys *ev_sys, execution_context_t *econtext, am_header_t *hdr, size_t hdr_len, void *data, size_t data_len)
@@ -96,6 +96,7 @@ int main(int argc, char **argv)
         fprintf(stderr, "[ERROR] get_config() failed\n");
         return EXIT_FAILURE;
     }
+    engine->config = &config_data;
 
     /*
      * INITIATE CONNECTION BETWEEN DPUS.
