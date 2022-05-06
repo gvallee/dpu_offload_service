8cb409b4d5b872c099a3c75080c17cd1bf8bafbe
Geoffroy Vallee <geoffroy.vallee@gmail.com>
2022-05-06 11:29:29 -0400

    Add test to simulate connection of many ranks on a single host

---
+++ daemons/single/single.c
@@ -0,0 +1,87 @@
+//
+// Copyright (c) 2022, NVIDIA CORPORATION. All rights reserved.
+//
+// See LICENSE.txt for license information
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "dpu_offload_service_daemon.h"
+
+/* This daemon simulate the execution of a single daemon on a single DPU.
+   This is mainly used for testing purposes. */
+
+// single <path_to_config_file> <ip> <port>
+int main(int argc, char **argv)
+{
+    ucp_params_t ucp_params;
+    ucp_worker_params_t worker_params;
+    ucs_status_t status;
+    ucp_config_t *config;
+    uint64_t my_dpu_id;
+    offloading_config_t dpu_cfg;
+    offloading_engine_t *engine = NULL;
+    execution_context_t *service_server = NULL;
+    init_params_t service_server_init_params;
+    conn_params_t service_server_conn_params;
+
+    if (argc != 3)
+    {
+        fprintf(stderr, "Please give in order:\n");
+        fprintf(stderr, "\t- the IP address to use,\n");
+        fprintf(stderr, "\t- the port to use.\n");
+        return EXIT_FAILURE;
+    }
+
+    offload_engine_init(&engine);
+    assert(engine);
+
+    // Manually initialize some aspects of the DPU since it is not a fully functional one
+    engine->on_dpu = true;
+    engine->num_dpus = 1;
+
+    /* NO INTER-DPUS CONNECTIONS */
+
+    /*
+     * CREATE A SERVER SO THAT PROCESSES RUNNING ON THE HOST CAN CONNECT.
+     */
+    fprintf(stderr, "Creating server for processes on the DPU\n");
+    // We let the system figure out the configuration to use to let ranks connect
+    RESET_INIT_PARAMS(&service_server_init_params);
+    RESET_CONN_PARAMS(&service_server_conn_params);
+    service_server_init_params.conn_params = &service_server_conn_params;
+    service_server_init_params.conn_params->addr_str = argv[1];
+    service_server_init_params.conn_params->port_str = argv[2];
+    service_server_init_params.conn_params->port = atoi(argv[2]);
+    SET_DEFAULT_DPU_HOST_SERVER_CALLBACKS(&service_server_init_params);
+    service_server = server_init(engine, &(service_server_init_params));
+    if (service_server == NULL)
+    {
+        fprintf(stderr, "service_server is undefined\n");
+        return EXIT_FAILURE;
+    }
+    ADD_SERVER_TO_ENGINE(service_server, engine);
+    fprintf(stderr, "server for application processes to connect has been successfully created\n");
+
+    /*
+     * PROGRESS UNTIL ALL PROCESSES ON THE HOST SEND A TERMINATION MESSAGE
+     */
+    fprintf(stderr, "%s: progressing...\n", argv[0]);
+    while (!EXECUTION_CONTEXT_DONE(service_server))
+    {
+        lib_progress(service_server);
+    }
+
+    // Finalizing
+    server_fini(&service_server);
+    offload_engine_fini(&engine);
+
+    return EXIT_SUCCESS;
+error_out:
+    if (service_server)
+        server_fini(&service_server);
+    if (engine)
+        offload_engine_fini(&engine);
+    return EXIT_FAILURE;
+}
---
+++ tests/offload_service/client/fake_mpi_rank.c
@@ -0,0 +1,108 @@
+//
+// Copyright (c) 2022, NVIDIA CORPORATION. All rights reserved.
+//
+// See LICENSE.txt for license information
+//
+
+/*
+ * The test fake_mpi_rank aims at testing the connection of multiple
+ * ranks on a *single node*. The group/rank information is provided
+ * via command line arguments. The test is not meant to be used with
+ * mpirun, just start processes with the appropriate arguments.
+ * The test is designed to be executed with the job_persistent_dpu_daemon.
+ * Use more than 1 fake rank.
+ */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "dpu_offload_service_daemon.h"
+
+// fake_mpi_rank <dpu_ip> <dpu_port> <group_id> <group_size> <rank>
+int main(int argc, char **argv)
+{
+    int rank;
+    int group_id;
+    int group_size;
+    char *dpu_addr;
+    int dpu_port;
+    dpu_offload_status_t rc;
+    offloading_engine_t *engine = NULL;
+    execution_context_t *client = NULL;
+    init_params_t client_init_params;
+    conn_params_t client_conn_params;
+    rank_info_t client_rank_info;
+
+    if (argc != 6)
+    {
+        fprintf(stderr,
+                "[ERROR] the test requires 3 arguments: %s <group_id> <group_size> <rank>\n",
+                argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    dpu_addr = argv[1];
+    dpu_port = atoi(argv[2]);
+    group_id = atoi(argv[3]);
+    group_size = atoi(argv[4]);
+    rank = atoi(argv[5]);
+
+    // Start the client for offloading
+    rc = offload_engine_init(&engine);
+    if (rc)
+    {
+        fprintf(stderr, "[ERROR] offload_engine_init() failed\n");
+        goto error_out;
+    }
+    if (engine == NULL)
+    {
+        fprintf(stderr, "[ERRO] undefined engine\n");
+        goto error_out;
+    }
+
+    RESET_INIT_PARAMS(&client_init_params);
+    RESET_CONN_PARAMS(&client_conn_params);
+    client_init_params.conn_params = &client_conn_params;
+    client_init_params.proc_info = &client_rank_info;
+    client_init_params.proc_info->group_id = group_id;
+    client_init_params.proc_info->group_rank = rank;
+    client_init_params.proc_info->group_size = group_size;
+    client_init_params.proc_info->n_local_ranks = group_size;
+    client_init_params.conn_params->addr_str = dpu_addr;
+    client_init_params.conn_params->port_str = argv[2];
+    client_init_params.conn_params->port = dpu_port;
+    client = client_init(engine, &client_init_params);
+    if (client == NULL)
+    {
+        fprintf(stderr, "[ERROR] client is undefined\n");
+        goto error_out;
+    }
+    engine->client = client;
+
+    // When only one rank is used, make sure the code ensures full initialization
+    // otherwise, the cache is full right away and client terminates too quickly
+    do
+    {
+        client->progress(client);
+    } while (client->client->bootstrapping.phase != BOOTSTRAP_DONE);
+
+    // Wait for full initialization to complete, i.e., until the group cache is fully populated
+    while (!group_cache_populated(engine, group_id))
+        offload_engine_progress(engine);
+    fprintf(stdout, "Rank %d: connection to DPU succeeded\n", rank);
+
+    // Finalizing
+    client_fini(&client);
+    offload_engine_fini(&engine);
+
+    fprintf(stdout, "%s: test succeeded\n", argv[0]);
+    return EXIT_SUCCESS;
+error_out:
+    if (client)
+        client_fini(&client);
+    if (engine)
+        offload_engine_fini(&engine);
+    fprintf(stderr, "%s: test failed\n", argv[0]);
+    return EXIT_FAILURE;
+}
