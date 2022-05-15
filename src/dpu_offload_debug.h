e54a15e94955de20ac3ffc8dbdd9fda70a114794
Geoffroy Vallee <geoffroy.vallee@gmail.com>
2022-05-15 14:51:08 -0700

    Disable debug messages when requested
    Signed-off-by: Geoffroy Vallee <geoffroy.vallee@gmail.com>

--- src/dpu_offload_debug.h
+++ src/dpu_offload_debug.h
@@ -19,22 +19,25 @@ typedef struct debug_config
     int verbose;
 } debug_config_t;
 extern debug_config_t dbg_cfg;
-#define DBG(_dbg_fmt, ...)                                         \
-    do                                                             \
-    {                                                              \
-        if (dbg_cfg.my_hostname == NULL)                           \
-        {                                                          \
-            dbg_cfg.my_hostname = malloc(1024);                    \
-            dbg_cfg.my_hostname[1023] = '\0';                      \
-            gethostname(dbg_cfg.my_hostname, 1023);                \
-            dbg_cfg.verbose = 1;                                   \
-            char *verbose_str = getenv(DPU_OFFLOAD_DBG_VERBOSE);   \
-            if (verbose_str)                                       \
-                dbg_cfg.verbose = atoi(verbose_str);               \
-        }                                                          \
-        fprintf(stdout, "[%s:l.%d:%s():%s:pid=%d] " _dbg_fmt "\n", \
-                __FILE__, __LINE__, __func__, dbg_cfg.my_hostname, \
-                getpid() __VA_OPT__(, ) __VA_ARGS__);              \
+#define DBG(_dbg_fmt, ...)                                             \
+    do                                                                 \
+    {                                                                  \
+        if (dbg_cfg.my_hostname == NULL)                               \
+        {                                                              \
+            dbg_cfg.my_hostname = malloc(1024);                        \
+            dbg_cfg.my_hostname[1023] = '\0';                          \
+            gethostname(dbg_cfg.my_hostname, 1023);                    \
+            dbg_cfg.verbose = 1;                                       \
+            char *verbose_str = getenv(DPU_OFFLOAD_DBG_VERBOSE);       \
+            if (verbose_str)                                           \
+                dbg_cfg.verbose = atoi(verbose_str);                   \
+        }                                                              \
+        if (dbg_cfg.verbose > 0)                                       \
+        {                                                              \
+            fprintf(stdout, "[%s:l.%d:%s():%s:pid=%d] " _dbg_fmt "\n", \
+                    __FILE__, __LINE__, __func__, dbg_cfg.my_hostname, \
+                    getpid() __VA_OPT__(, ) __VA_ARGS__);              \
+        }                                                              \
     } while (0)
 #else
 #define DBG(...) \
