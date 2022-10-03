042edb445859be838b2225d3aaec0a2281a7602a
Geoffroy Vallee <geoffroy.vallee@gmail.com>
2022-10-03 14:16:54 -0400

    Fix mistakes i did during rebase

--- src/dpu_offload_event_channels.c
+++ src/dpu_offload_event_channels.c
@@ -116,14 +116,9 @@ static void am_rdv_recv_cb(void *request, ucs_status_t status, size_t length, vo
 {
     assert(status == UCS_OK);
     pending_am_rdv_recv_t *recv_info = (pending_am_rdv_recv_t *)user_data;
-<<<<<<< HEAD
-    // Find associated execution context
-    assert(recv_info->engine);
-=======
     assert(recv_info);
     assert(recv_info->engine);
     // Find associated execution context
->>>>>>> 812dd44 (Moving the AM recv handler to the engine level)
     execution_context_t *econtext = NULL;
     dpu_offload_status_t ret = get_associated_econtext(recv_info->engine, recv_info->hdr, &econtext);
     if (ret != DO_SUCCESS)
@@ -148,7 +143,7 @@ static void am_rdv_recv_cb(void *request, ucs_status_t status, size_t length, vo
         }
         else
         {
-            if (engine->settings.buddy_buffer_system_enabled)
+            if (recv_info->engine->settings.buddy_buffer_system_enabled)
             {
                 SMART_BUFF_RETURN(&(recv_info->engine->smart_buffer_sys),
                                   recv_info->payload_size,
