e89bdb186f956d0c9e25036b5e893927e2792796
Geoffroy Vallee <geoffroy.vallee@gmail.com>
2023-01-03 12:38:57 -0800

    Fix get the SP EP from host when using a single rank
    Signed-off-by: Geoffroy Vallee <geoffroy.vallee@gmail.com>

--- src/dpu_offload_utils.c
+++ src/dpu_offload_utils.c
@@ -264,6 +264,7 @@ dpu_offload_status_t send_revoke_group_rank_request_through_num_ranks(execution_
 bool group_cache_populated(offloading_engine_t *engine, group_uid_t gp_uid)
 {
     group_cache_t *gp_cache = GET_GROUP_CACHE(&(engine->procs_cache), gp_uid);
+    assert(gp_cache);
     if (gp_cache->global_revoked == 0 && gp_cache->group_size == gp_cache->num_local_entries)
     {
         DBG("Group cache for group 0x%x fully populated. num_local_entries = %" PRIu64 " group_size = %" PRIu64,
@@ -1190,6 +1191,11 @@ dpu_offload_status_t get_sp_ep_by_id(offloading_engine_t *engine, uint64_t sp_id
 
     // If not, we find the appropriate client or server
     *sp_ep = GET_REMOTE_SERVICE_PROC_EP(engine, sp_id);
+    if (*sp_ep == NULL && !engine->on_dpu && sp_id == engine->config->local_service_proc.info.global_id)
+    {
+        // EP not in cache (it happens) but the local SP is requested
+        *sp_ep = engine->client->client->server_ep;
+    }
     *econtext_comm = GET_REMOTE_SERVICE_PROC_ECONTEXT(engine, sp_id);
     switch ((*econtext_comm)->type)
     {
