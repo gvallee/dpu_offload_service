1ce9dfc77329d6811b94733451d6c1c21cd18377
Geoffroy Vallee <geoffroy.vallee@gmail.com>
2023-01-19 15:21:12 -0500

    Finish the documentation for the upcoming APIs

--- src/dpu_offload_group_cache.h
+++ src/dpu_offload_group_cache.h
@@ -111,28 +111,90 @@ dpu_offload_status_t get_num_ranks_for_group_host(offloading_engine_t *engine,
                                                   host_info_t host_id,
                                                   size_t *num_ranks);
 
+/**
+ * @brief Get the index associated to a rank for the contiguous, sorted array of ranks running on the target host.
+ * 
+ * @param[in] engine Associated offload engine
+ * @param[in] group_uid Target group identified by its unique group identifier
+ * @param[in] host_id Target host identifier
+ * @param[in] rank Target rank in the group
+ * @param[in,out] idx Returned index associated to the rank from the array of ranks running on the host.
+ * If the rank is not running on the host or beyond the group size, an internal error is raised
+ * @return dpu_offload_status_t 
+ */
 dpu_offload_status_t get_n_for_rank_by_group_host(offloading_engine_t *engine,
                                                   group_uid_t group_uid,
-                                                  host_info_t my_host_id,
+                                                  host_info_t host_id,
+                                                  int64_t rank,
                                                   int64_t *idx);
 
+/**
+ * @brief Get all the service processes involed in a group that are associated to a specific host
+ * 
+ * @param[in] engine Associated offload engine
+ * @param[in] group_uid Target group identified by its unique group identifier
+ * @param[in] host_id Target host identifier
+ * @param[in,out] sps Pointer to the internal lookup array associated to the list of service processes for the target host.
+ * Since the table is exposed directly from the cache, the caller must not free the array, it will be freed when the group cache is freed.
+ * If no service process on the target host is involved in the group, the pointer is set to NULL and no exception is raised.
+ * @param[in,out] num_sps Number of service processes associated to the target host within the group;
+ * 0 if no service process on the host is involved in the group.
+ * @return dpu_offload_status_t 
+ */
 dpu_offload_status_t get_all_sps_by_group_host(offloading_engine_t *engine,
                                                group_uid_t group_uid,
                                                host_info_t host_id,
                                                dyn_array_t *sps,
                                                size_t *num_sps);
 
+/**
+ * @brief Get all the hosts involved in a group. The resulting array is contiguous and ordered.
+ * Ordering is based on the configuration that is loaded at bootstrapping time; in most cases
+ * based on the content of the configuration file used for the job.
+ * 
+ * @param[in] engine Associated offload engine
+ * @param[in] group_uid Target group identified by its unique group identifier
+ * @param[in,out] hosts Pointer to the internal lookup array associated to the list of hosts that are involved in the group.
+ * The caller must not free the array at any point since it is an internal array directly exposed to users. It is freed when
+ * the group cache is being freed.
+ * @param[in,out] num_hosts Number of hosts involed in the group. The returned value must strictly be greater than 0 since groups
+ * are created only when a rank from the group triggers its creatiom, guaranteeing that all known groups must have a minimum of 1 rank.
+ * @return dpu_offload_status_t 
+ */
 dpu_offload_status_t get_all_hosts_by_group(offloading_engine_t *engine,
                                             group_uid_t group_uid,
                                             dyn_array_t *hosts,
                                             size_t *num_hosts);
 
+/**
+ * @brief Get all the ranks associated to a specific service process involed in a given group.
+ * 
+ * @param[in] engine Associated offload engine
+ * @param[in] group_uid Target group identified by its unique group identifier
+ * @param[in] sp_group_gid Global group service process identifer, as for example returned by get_local_sp_id_by_group(). Must be valid.
+ * @param[in,out] ranks Pointer to the internal lookup array associated to the list of ranks associated with a given service process.
+ * The caller must not free the array at any point since it is an internal array directly exposed to users. It is freed when
+ * the group cache is being freed.
+ * @param[in,out] num_ranks Number of ranks associated to the target service process. The returned value must strictly be greater than
+ * 0 since the global group service identifier is assumed valid and therefore a strict minimum of one rank must be associated to the
+ * service process (otherwise the global group service process would be invalid)
+ * @return dpu_offload_status_t 
+ */
 dpu_offload_status_t get_all_ranks_by_group_sp(offloading_engine_t *engine,
                                                group_uid_t group_uid,
-                                               uint64_t remote_global_group_sp_id,
+                                               uint64_t sp_group_gid,
                                                dyn_array_t *ranks,
                                                size_t *num_ranks);
 
+/**
+ * @brief Get the n^th service process on a given host that is involved in the target group.
+ * 
+ * @param[in] engine Associated offload engine
+ * @param[in] group_uid Target group identified by its unique group identifier
+ * @param[in] host_id Target host identifier
+ * @param[in,out] sp_group_gid Global group identifier of the target service process
+ * @return dpu_offload_status_t 
+ */
 dpu_offload_status_t get_nth_sp_by_group_host(offloading_engine_t *engine,
                                               group_uid_t group_uid,
                                               host_info_t host_id,
