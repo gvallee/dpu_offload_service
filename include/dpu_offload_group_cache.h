55f3a2c9cac80f5e9507b719da31e2487c5ee378
Geoffroy Vallee <geoffroy.vallee@gmail.com>
2023-03-16 12:56:27 -0700

    Fix bug in group cache finalization
    Signed-off-by: Geoffroy Vallee <geoffroy.vallee@gmail.com>

--- include/dpu_offload_group_cache.h
+++ include/dpu_offload_group_cache.h
@@ -6,6 +6,61 @@
 
 #ifndef DPU_OFFLOAD_GROUP_CACHE_H_
 
+/* GROUPS_CACHE_INIT initializes the cache that holds information about all the groups */
+#define GROUPS_CACHE_INIT(_cache)                                                            \
+    do                                                                                       \
+    {                                                                                        \
+        RESET_CACHE(_cache);                                                                 \
+        (_cache)->data = kh_init(group_hash_t);                                              \
+        DYN_LIST_ALLOC((_cache)->group_cache_pool, DEFAULT_NUM_GROUPS, group_cache_t, item); \
+    } while (0)
+
+#define GROUPS_CACHE_FINI(_cache)                                       \
+    do                                                                  \
+    {                                                                   \
+        int key;                                                        \
+        group_cache_t *value = NULL;                                    \
+        kh_foreach((_cache)->data, key, value, {                        \
+            if (value != NULL)                                          \
+            {                                                           \
+                group_cache_t *_gp_cache = NULL;                        \
+                _gp_cache = GET_GROUP_CACHE((_cache), key);             \
+                assert(_gp_cache);                                      \
+                /* avoid compile time warnings */                       \
+                if (_gp_cache->rank_array_initialized)                  \
+                {                                                       \
+                    DYN_ARRAY_FREE(&(_gp_cache->ranks));                \
+                    _gp_cache->rank_array_initialized = false;          \
+                }                                                       \
+                if (_gp_cache->sp_array_initialized)                    \
+                {                                                       \
+                    DYN_ARRAY_FREE(&(_gp_cache->sps));                  \
+                    _gp_cache->sp_array_initialized = false;            \
+                }                                                       \
+                if (_gp_cache->host_array_initialized)                  \
+                {                                                       \
+                    DYN_ARRAY_FREE(&(_gp_cache->hosts));                \
+                    _gp_cache->host_array_initialized = false;          \
+                }                                                       \
+                /* Free hash table(s) */                                \
+                GROUP_CACHE_HASHES_FINI((_cache)->engine, _gp_cache);   \
+                /* Free the bitset for SPs */                           \
+                GROUP_CACHE_BITSET_DESTROY(_gp_cache->sps_bitset);      \
+            }                                                           \
+        }) kh_destroy(group_hash_t, (_cache)->data);                    \
+        DYN_LIST_FREE((_cache)->group_cache_pool, group_cache_t, item); \
+        (_cache)->group_cache_pool = NULL;                              \
+        (_cache)->size = 0;                                             \
+    } while (0)
+
+/* GROUP_CACHE_INIT initializes the cache for a given group */
+#define GROUP_CACHE_INIT(_cache, _gp_id)                                     \
+    do                                                                       \
+    {                                                                        \
+        void *_gp_cache = _cache[_gp_id];                                    \
+        DYN_ARRAY_ALLOC((dyn_array_t *)_gp_cache, 2048, peer_cache_entry_t); \
+    } while (0)
+
 /**
  * @brief This function populated the cache's lookup tables. It assumes the cache is
  * fully ppopulated.
